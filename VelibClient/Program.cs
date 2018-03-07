ab2f4069f0ac5e95873dfc4e096a2650066dbc5f
Sticmac <jujulem06@hotmail.fr>
2018-03-07 16:21:29 +0100

    First version of Velib Client
    Lists all stations' names in Toulouse.

---
+++ VelibClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VelibClient.VelibService;
+
+namespace VelibClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VelibServiceClient velibService = new VelibServiceClient();
+
+            Station[] stations = velibService.GetStations("Toulouse");
+
+            foreach(Station station in stations)
+            {
+                Console.WriteLine(station.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
